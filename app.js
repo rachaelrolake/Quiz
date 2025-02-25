df81d3361a183b96466cd68e32e3a0f4e0a7f6b7
Rachael Rolake Olaosun <rachaelrolake@gmail.com>
2025-02-26 00:39:34 +0100

    update

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const questions = [
       'b) Cornrows',
       'c) Loose afro',
       'd) Ponytail'],
-    correct: 'b) Cornrows'
+    correct: 'b) Cornrows',
+    imageLeft: 'images/Curly hair-pana1.png',
+    imageRight: 'images/Curly hair-bro.png'
   },
   {
-    question: 'What is the purpose of sleeping with a silk or satin bonnet?',
+    question: 'What is the purpose of sleeping with a silk/satin bonnet?',
     options: ['a) To make hair grow faster',
       'b) To keep hair soft and reduce breakage',
       'c) To make hair shinier',
       'd) To prevent dandruff'],
-    correct: 'b) To keep hair soft and reduce breakage'
+    correct: 'b) To keep hair soft and reduce breakage',
+    imageLeft: 'images/messy bun-cuate.png',
+    imageRight: 'images/Hair spray-bro.png'
   },
   {
     question: 'What heat styling tool is least damaging to natural hair?',
@@ -21,7 +25,9 @@ const questions = [
       'b) Blow dryer with diffuser',
       'c) Curling wand',
       'd) Crimping iron'],
-    correct: 'b) Blow dryer with diffuser'
+    correct: 'b) Blow dryer with diffuser',
+    imageLeft: 'images/hairdresser team-cuate.png',
+    imageRight: 'images/hairdresser team-amico.png'
   },
   {
     question: 'Which of these oils is best for scalp massages to promote hair growth?',
@@ -29,7 +35,9 @@ const questions = [
       'b) Jamaican black castor oil',
       'c) Petroleum jelly',
       'd) Baby oil'],
-    correct: 'b) Jamaican black castor oil'
+    correct: 'b) Jamaican black castor oil',
+    imageLeft: 'images/messy bun-pana.png',
+    imageRight: 'images/messy bun-rafiki.png'
   },
   {
     question: 'What is the biggest sign of protein overload in hair?',
@@ -37,15 +45,9 @@ const questions = [
       'b) Hair feels dry, brittle, and snaps easily',
       'c) Hair looks shinier than usual',
       'd) Hair grows faster'],
-    correct: 'b) Hair feels dry, brittle, and snaps easily'
-  },
-  {
-    question: 'Which hair type is most prone to dryness?',
-    options: ['a) Straight hair',
-      'b) Wavy hair',
-      'c) Curly hair',
-      'd) Coily/kinky hair'],
-    correct: 'd) Coily/kinky hair'
+    correct: 'b) Hair feels dry, brittle, and snaps easily',
+    imageLeft: 'images/Hair-pulling disorder-bro.png',
+    imageRight: 'images/Hair-pulling disorder-amico.png'
   },
   {
     question: 'What is the best way to keep your scalp healthy?',
@@ -53,7 +55,9 @@ const questions = [
       'b) Apply heavy oils daily',
       'c) Exfoliate, cleanse, and moisturize when needed',
       'd) Avoid touching it'],
-    correct: 'c) Exfoliate, cleanse, and moisturize when needed'
+    correct: 'c) Exfoliate, cleanse, and moisturize when needed',
+    imageLeft: 'images/Curly hair-rafiki.png',
+    imageRight: 'images/Curly hair-pana.png'
   },
   {
     question: 'Which ingredient is a natural humectant that draws moisture into the hair?',
@@ -61,13 +65,17 @@ const questions = [
       'b) Aloe vera',
       'c) Beeswax',
       'd) Jojoba oil'],
-    correct: 'b) Aloe vera'
+    correct: 'b) Aloe vera',
+    imageLeft: 'images/messy bun-bro.png',
+    imageRight: 'images/aloe vera-bro.png'
   },
   {
-    question: 'True or False: Trimming your hair makes it grow faster.',
+    question: 'Trimming your hair makes it grow faster.',
     options: ['a) True ',
       'b) False'],
-    correct: 'b) False'
+    correct: 'b) False',
+    imageLeft: 'images/Hairdresser-pana.png',
+    imageRight: 'images/hairdresser team-broq.png'
   },
   {
     question: 'Which hair porosity type absorbs moisture quickly but loses it fast?',
@@ -75,7 +83,9 @@ const questions = [
       'b) Medium porosity',
       'c) High porosity',
       'd) All hair types'],
-    correct: 'c) High porosity'
+    correct: 'c) High porosity',
+    imageLeft: 'images/Fire hair-rafiki.png',
+    imageRight: 'images/Fire hair-bro.png'
   },
   {
     question: 'What is a sign that your hair is over-moisturized (hygral fatigue)?',
@@ -83,7 +93,9 @@ const questions = [
       'b) Breaks easily and feels dry',
       'c) Looks dull but feels soft',
       'd) Shrinks more than usual'],
-    correct: 'a) Feels mushy and limp'
+    correct: 'a) Feels mushy and limp',
+    imageLeft: 'images/Hairdresser-cuate.png',
+    imageRight: 'images/Hair-pulling disorder-cuate.png'
   },
   {
     question: 'Which technique helps define curls the most?',
@@ -91,7 +103,9 @@ const questions = [
       'b) Using a wide-tooth comb on dry hair',
       'c) Applying gel or curl cream on wet hair using raking or shingling',
       'd) Blow-drying on high heat'],
-    correct: 'c) Applying gel or curl cream on wet hair using raking or shingling'
+    correct: 'c) Applying gel or curl cream on wet hair using raking or shingling',
+    imageLeft: 'images/Hair spray-pana.png',
+    imageRight: 'images/women in hair salon-cuate.png'
   },
   {
     question: 'Why do protective styles help with hair growth?',
@@ -99,7 +113,9 @@ const questions = [
       'b) They reduce manipulation and breakage',
       'c) They make hair grow faster',
       'd) They stop shrinkage'],
-    correct: 'b) They reduce manipulation and breakage'
+    correct: 'b) They reduce manipulation and breakage',
+    imageLeft: 'images/Hairdresser-pana.png',
+    imageRight: 'images/Hair-pulling disorder-bro.png'
   },
   {
     question: 'What’s the best way to keep moisture in braids or twists?',
@@ -107,7 +123,9 @@ const questions = [
       'b) Spray a water-based moisturizer and seal with oil',
       'c) Avoid washing the style',
       'd) Only oil the scalp'],
-    correct: 'b) Spray a water-based moisturizer and seal with oil'
+    correct: 'b) Spray a water-based moisturizer and seal with oil',
+    imageLeft: 'images/Hair spray-pana.png',
+    imageRight: 'images/Hair spray-amico.png'
   },
   {
     question: 'How much does hair grow on average per month?',
@@ -115,7 +133,9 @@ const questions = [
       'b) ½ inch (1.2 cm)',
       'c) 1 inch (2.5 cm)',
       'd) 2 inches (5 cm)'],
-    correct: 'b) ½ inch (1.2 cm)'
+    correct: 'b) ½ inch (1.2 cm)',
+    imageLeft: 'images/Barber-bro.png',
+    imageRight: 'images/Depilation-bro.png'
   },
   {
     question: 'Which vitamin is often linked to hair loss prevention?',
@@ -123,13 +143,17 @@ const questions = [
       'b) Vitamin B7 (Biotin)',
       'c) Vitamin C',
       'd) Vitamin D'],
-    correct: 'b) Vitamin B7 (Biotin)'
+    correct: 'b) Vitamin B7 (Biotin)',
+    imageLeft: 'images/women in hair salon-bro.png',
+    imageRight: 'images/women in hair salon-rafiki.png'
   },
   {
-    question: 'True or False: Drinking more water can help with hair hydration.',
+    question: 'Drinking more water can help with hair hydration.',
     options: ['a) True ',
       'b) False'],
-    correct: 'b) False'
+    correct: 'b) False',
+    imageLeft: 'images/messy bun-amico.png',
+    imageRight: 'images/Curly hair-rafiki.png'
   },
   {
     question: 'What is the LCO method for moisturizing hair?',
@@ -137,7 +161,9 @@ const questions = [
       'b) Leave-in → Conditioner → Oil',
       'c) Liquid → Cream → Oil',
       'd) Lotion → Castor oil → Olive oil'],
-    correct: 'c) Liquid → Cream → Oil'
+    correct: 'c) Liquid → Cream → Oil',
+    imageLeft: 'images/women in hair salon-rafiki.png',
+    imageRight: 'images/women in hair salon-pana.png'
   },
   {
     question: 'How often should you clarify your hair to remove product buildup?',
@@ -145,16 +171,11 @@ const questions = [
       'b) Once a month',
       'c) Every 2-4 weeks',
       'd) Never'],
-    correct: 'c) Every 2-4 weeks'
+    correct: 'c) Every 2-4 weeks',
+    imageLeft: 'images/Curly hair-pana1.png',
+    imageRight: 'images/Hair spray-bro.png'
   },
-  {
-    question: 'What is the best way to detangle curly hair?',
-    options: ['a) Dry with a fine-tooth comb',
-      'b) Wet with conditioner and a wide-tooth comb',
-      'c) Brushing when damp with a boar bristle brush',
-      'd) Raking fingers through dry hair'],
-    correct: 'b) Wet with conditioner and a wide-tooth comb'
-  }
+
 ];
 
 let currentQuestionIndex = 0;
@@ -166,13 +187,16 @@ function loadQuestion() {
   const optionsContainer = document.getElementById("options");
   optionsContainer.innerHTML = "";
 
+  document.querySelector(".left-side").src = questions[currentQuestionIndex].imageLeft;
+  document.querySelector(".right-side").src = questions[currentQuestionIndex].imageRight;
+
   questions[currentQuestionIndex].options.forEach(option => {
     const btn = document.createElement("button");
     btn.textContent = option;
     btn.classList.add("option");
 
     if (selectedAnswers[currentQuestionIndex]) {
-      btn.disabled = true; // Disable option if already chosen
+      btn.disabled = true;
       if (option === selectedAnswers[currentQuestionIndex].option) {
         btn.classList.add(selectedAnswers[currentQuestionIndex].isCorrect ? "correct" : "wrong");
       }
@@ -192,13 +216,15 @@ function loadQuestion() {
 
 function checkAnswer(btn, option) {
   const correctAnswer = questions[currentQuestionIndex].correct;
-    const isCorrect = option === correctAnswer;
-    btn.classList.add(isCorrect ? "correct" : "wrong");
-  
-    // Store the selected answer & correctness
-    selectedAnswers[currentQuestionIndex] = { option, isCorrect };
-  
-    if (isCorrect) score++;
+  document.querySelectorAll(".option").forEach(button => button.disabled = true);
+
+
+  const isCorrect = option === correctAnswer;
+  btn.classList.add(isCorrect ? "correct" : "wrong");
+
+  selectedAnswers[currentQuestionIndex] = { option, isCorrect };
+
+  if (isCorrect) score++;
 }
 
 document.getElementById("next").onclick = () => {
